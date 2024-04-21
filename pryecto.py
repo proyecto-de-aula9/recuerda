79e9558f97764f29ec0d8cf1d8b825af5d369ee9
bryan689 <bryandrez2006@gmail.com>
2024-04-21 14:10:20 -0500

    V.0.3
    ya se para que es el for
    se agregaron funciones nuevas como ejecutar_alarma y medicamento_tomado
    ya se pueden programar varias alarmas y hacer otros porcesos gracias a la funcion ejcutar_alarma ya que trabaja con hilos y la funcion medicamento_tomado no funciona(corregir)

--- pryecto.py
+++ pryecto.py
@@ -1,27 +1,41 @@
-funciones = {}
-
+import threading
 import time
 
+funciones = {}
+
 def agregar_medicamento(nombre,horas,minutos):
-    funciones[nombre] = horas
+    funciones[nombre] = {"frecuencia":(horas,minutos),"alarma_activa": True}
     print("medicamento agregado correcta mente")
-    tiempo = horas *60 *60 + minutos*60
-    print(f"la alarma sonara en {horas} horas y {minutos} minutos ")
-    time.sleep(tiempo)
-    print("hora de los medicamentos")
+
+    def ejecutar_alarma(nombre, horas, minutos):
+        while True:
+            tiempo = horas * 3600 + minutos
+            time.sleep(tiempo)
+            print(f"Hora de tomar el medicamento: {nombre}")
+
+    # Crear y comenzar un nuevo hilo para la alarma
+    hilo_alarma = threading.Thread(target=ejecutar_alarma, args=(nombre, horas, minutos))
+    hilo_alarma.start()
     
 
 def listado_medicamentos():
     print("\n** listado de medicamentos **")
-    #for nombre, horas in funciones.items():
-    print(f"{nombre}, cada {horas} horas y {minutos} minutos")
+    for nombre, datos in funciones.items():
+        print(f"{nombre}, cada {datos["frecuencia"][0]} horas y {datos["frecuencia"][1]} minutos")
+
+def medicamento_tomado(nombre):
+    if nombre in funciones :
+        funciones[nombre]["alarma_activa"] = False
+        print(f"medicamento {nombre} marcado como tomado")
+    else :
+        print("no se encontro el medicamento {nombre} en la lista de medicamentos")
 
 
 
 while True :
     print("opcion 1: agregar medicamento")
     print("opcion 2: mostrar listado de medicamentos")
-    print("opcion 3: historial de medicamentos tomados")
+    print("opcion 3: marcar medicamento como tomado")
     print("opcion 4: salir")
     op = input("que opcion elije ")
     if op == "1":
@@ -32,6 +46,10 @@ while True :
     
     elif op == "2":
         listado_medicamentos()
+
+    elif op == "3":
+        nombre = input("ingrese el nombre del medicamento que ya tomo ")
+        medicamento_tomado(nombre)
 
     elif op == "4":
         break
